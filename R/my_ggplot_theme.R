46e12e5914084d7d83581e3d8265ef9ae8fdee00
Giulio Caravagna <gcaravagn@gmail.com>
2020-02-25 08:46:55 +0100

    Update my_ggplot_theme.R

--- R/my_ggplot_theme.R
+++ R/my_ggplot_theme.R
@@ -1,6 +1,8 @@
 my_ggplot_theme = function(cex = 1)
 {
-  theme_light(base_size = 10 * cex) +
+  cex_opt = getOption('bmix_cex', default = 1)
+
+  theme_light(base_size = 10 * cex_opt) +
     theme(
       legend.position = "bottom",
       legend.key.size = unit(.3 * cex, "cm"),
